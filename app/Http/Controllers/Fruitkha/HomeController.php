ea200fca8c4738c4f37b3f24929184144f14aa8c
dragondl48 <daohongphuc1999@gmail.com>
2023-04-06 21:09:35 +0700

    paginate and list Product

---
+++ app/Http/Controllers/Fruitkha/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Fruitkha;
+
+use App\Http\Controllers\Controller;
+use App\Http\Repository\ProductRepository;
+
+class HomeController extends Controller
+{
+    protected $productRepo;
+    function __construct(ProductRepository $productRepo)
+    {
+        $this->productRepo=$productRepo;
+    }
+    function homeViewV1()
+    {
+        $products=$this->getProductTop3();
+        return view('client.home.index',compact("products"));
+    }
+
+    function homeViewV2()
+    {
+        return view('client.home.index-2');
+    }
+    function aboutUsView(){
+        return view('client.about-us.about-us');
+    }
+    function contactView(){
+        return view('client.contact.contact');
+    }
+    function getProductTop3(){
+        $products=$this->productRepo->getTop3Product();
+        return $products;
+    }
+}
---
+++ app/Http/Repository/ProductRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Repository;
+use App\Models\Products;
+use Illuminate\Cache\RateLimiting\Limit;
+
+class ProductRepository
+{
+    function getTop3Product()
+    {
+        $products=Products::select()->orderBy('created_at','ASC')->limit(3)->get();
+        return $products;
+    }
+
+    function getAllOfProduct(){
+        $products=Products::select()->orderBy('created_at','ASC')->paginate(5);
+        return $products;
+    }
+}
