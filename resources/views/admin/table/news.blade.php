5305408df65d6dcca896448eb66e924454127e32
Tran Quang Anh <quanh@Trans-MacBook-Pro.local>
2023-04-11 20:07:08 +0700

    update new

--- resources/views/admin/table/news.blade.php
+++ resources/views/admin/table/news.blade.php
@@ -79,16 +79,6 @@
                                     </tr>
                                 @endforeach
                                 </tbody>
-                                <tfoot>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Office</th>
-                                    <th>Age</th>
-                                    <th>Start date</th>
-                                    <th>Salary</th>
-                                </tr>
-                                </tfoot>
                             </table>
                         </div>
                     </div>
